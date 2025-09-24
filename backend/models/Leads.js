9211609dcd37cdb39184ade64612d058243cf280
SuhailDev28 <156823787+SuhailDev28@users.noreply.github.com>
2025-09-24 16:46:00 +0300

    Delete backend directory

--- backend/models/Leads.js
+++
@@ -1,8 +0,0 @@
-// models/Lead.js
-const mongoose = require('mongoose');
-
-const leadSchema = new mongoose.Schema({
-    clickCount: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Lead', leadSchema);
