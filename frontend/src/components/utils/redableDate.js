aaa0fe667cefd2fefc66de6ba0a6e02d28786d4b
SuhailDev28 <156823787+SuhailDev28@users.noreply.github.com>
2025-09-24 16:46:13 +0300

    Delete frontend directory

--- frontend/src/components/utils/redableDate.js
+++
@@ -1,9 +0,0 @@
-export const toRedableDate = (datetoconvert) => {
-  const nDate = new Date(datetoconvert);
-  return nDate.toLocaleDateString();
-};
-
-export const toRedableDateAndTime = (dateAndTimeToConvert) => {
-  const newDate = new Date(dateAndTimeToConvert);
-  return newDate.toLocaleString();
-};
